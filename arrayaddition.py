b3ff8204062ace112a05c6f45a763c8177d51e36
Shuvo <suvo10.info@gmail.com>
2025-06-19 01:11:02 +0600

    This is my journey to numpy buddy

---
+++ arrayaddition.py
@@ -0,0 +1,6 @@
+import numpy as np
+first_array=np.array([1,3,5,7])
+second_array=np.array([2,4,6,8])
+result1=first_array+second_array
+print("Using the operator :",result1)
+result2=np.add(first_array,second_array)
