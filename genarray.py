7cdf7123dbc1547cbdcf49173025bb98d32efd67
Shuvo <suvo10.info@gmail.com>
2025-06-25 20:02:25 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ genarray.py
@@ -0,0 +1,7 @@
+import numpy as py
+integer_array=np.random.randit(0,10,5)
+print(" 1D Random Integer Array:\n",integer_array)
+float_array=np.random.rand(5)
+print("\n1D Random Float Array:\n",float_array")
+result=np.random.randint(0,10,(3,4))
+print("\2D Random Integer Array:\n",result)
