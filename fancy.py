7e85f1d48e8d68080cd8c9a101e57a73589ed0b1
Shuvo <suvo10.info@gmail.com>
2025-06-25 18:49:39 +0600

    Merge remote-tracking branch 'origin/master'

--- fancy.py
+++ fancy.py
@@ -8,3 +8,16 @@ select_elements = array1[[1, 2, 5, 7]]
 
 print(select_elements)
 
+simple_indexing = array1[3]
+print("simple Indexing :",simple_indexing)
+fancy_indexing=array1[[1,2,5,7]]
+print("Fancy Indexing:",fancy_indexing) # [2 3 6,8]
+
+sorted_array=array1[np.argsort(array1)]
+print(sorted_array)
+sorted_array1=array1[np.argsort(-array1)]
+print(sorted_array1)
+indices=[1,3,6]
+new_values = [10,20,30]
+array1[indices]=new_values
+print(array1)
