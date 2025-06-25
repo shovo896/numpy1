2567dce8ed9bc9d3b2a301a8daa994d489a5bd0a
Shuvo <suvo10.info@gmail.com>
2025-06-25 18:36:05 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ fancy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# create a numpy array
+array1 = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+# select elements at index 1, 2, 5, 7
+select_elements = array1[[1, 2, 5, 7]]
+
+print(select_elements)
+
