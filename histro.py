894d8737c5d89b261bece63df3d714c283b0b12c
Shuvo <suvo10.info@gmail.com>
2025-06-27 01:44:52 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ histro.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+weight=np.array([0.48,1.7,2,3])
+plt.hist(weight)
+plt.show()
---
+++ universal.py
@@ -0,0 +1,7 @@
+import numpy as np
+angles = np.array([0,1,2])
+print("Angles:",angles)
+sine_values=np.sin(angles)
+print("sine values:",sine_values)
+inverse_sine=np.arcsin(angles)
+print("Inverse sine values :",inverse_sine)
